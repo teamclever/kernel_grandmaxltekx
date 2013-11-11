b08790a267eb5bb75bad3da2c51ca95c98c6d429
yank555-lu <yank555.lu@gmail.com>
2013-11-11 01:48:32 -0600

    kernel/power/powersuspend: remove userspace dependency from powersuspend
    make powersuspend not depend on a userspace initiator anymore, but use a hook
    in autosleep instead.
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    	수정함:        include/linux/powersuspend.h
    	수정함:        kernel/power/autosleep.c
    	수정함:        kernel/power/powersuspend.c

--- kernel/power/autosleep.c
+++ kernel/power/autosleep.c
@@ -13,6 +13,10 @@
 #include <linux/secgpio_dvs.h>
 #endif
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #include "power.h"
 
 static suspend_state_t autosleep_state;
@@ -127,8 +131,14 @@ int pm_autosleep_set_state(suspend_state_t state)
 	if (state > PM_SUSPEND_ON) {
 		pm_wakep_autosleep_enabled(true);
 		queue_up_suspend_work();
+#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_hook(POWER_SUSPEND_ACTIVE); // Yank555.lu : add hook to handle powersuspend tasks
+#endif
 	} else {
 		pm_wakep_autosleep_enabled(false);
+#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_hook(POWER_SUSPEND_INACTIVE); // Yank555.lu : add hook to handle powersuspend tasks
+#endif
 	}
 
 	mutex_unlock(&autosleep_lock);
