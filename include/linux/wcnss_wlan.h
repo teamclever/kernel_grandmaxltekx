5dbaf7b59779d4c44178e68273dc3d8ae96d7479
gugu0das <gugu0das@gmail.com>
2017-10-31 02:27:47 +0900

    wcnss: Define wcnss_get_iris_name

--- include/linux/wcnss_wlan.h
+++ include/linux/wcnss_wlan.h
@@ -106,6 +106,7 @@ void wcnss_riva_dump_pmic_regs(void);
 int wcnss_xo_auto_detect_enabled(void);
 u32 wcnss_get_wlan_rx_buff_count(void);
 int wcnss_wlan_iris_xo_mode(void);
+int wcnss_get_iris_name(char *iris_version);
 #ifdef CONFIG_WCNSS_REGISTER_DUMP_ON_BITE
 void wcnss_log_debug_regs_on_bite(void);
 #else
