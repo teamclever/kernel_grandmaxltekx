2e61bb5150d0ff96288273a5ef44e516bb58f437
kongwoojin <kongwoojin@naver.com>
2017-12-27 18:18:03 +0900

    build_kernel.sh: Auto repack bootimg and make flashable zip

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,9 +2,26 @@
 
 export ARCH=arm
 export CROSS_COMPILE=$(pwd)/arm-eabi-4.8/bin/arm-eabi-
-mkdir output
 
+# Check output directory
+if [ ! -d "output" ] 
+then
+    mkdir output
+fi
+
+echo "Start build..."
 make -C $(pwd) O=output msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_grandmax_koropen_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -C $(pwd) O=output
 
-cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+echo "Strip Prima wlan drivers..."
+./arm-eabi-4.8/bin/arm-eabi-strip --strip-unneeded $(pwd)/output/drivers/staging/prima/wlan.ko
+cp output/drivers/staging/prima/wlan.ko utilities/zip/system/lib/modules/pronto/pronto_wlan.ko
+echo "Finish"
+
+echo "Repack bootimg..."
+./utilities/repack/mkbootimg --cmdline "console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci" --base 0x80000000 --pagesize 2048 --kernel output/arch/arm/boot/zImage --ramdisk utilities/repack/boot.img-ramdisk.gz --dt utilities/repack/boot.img-dtb -o utilities/zip/boot.img
+echo "Finish"
+
+echo "Make flashable zip..."
+zip -r9 Clever-Kernel-grandmaxltekx.zip utilities/zip/META-INF utilities/zip/system utilities/zip/boot.img
+echo "Finish"
